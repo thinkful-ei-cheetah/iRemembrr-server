14dd46f2d69cbca57407eeb58beeb303b9c35b7f
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-06-25 11:31:22 -0500

    work start on /guess post request

---
+++ src/Algorithms/LinkedList.js
@@ -0,0 +1,72 @@
+'use strict';
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+  find(item) {
+    let currNode = this.head;
+
+    if (!this.head) {
+      return null;
+    }
+
+    while (currNode.value !== item) {
+      if (currNode.next === null) {
+        return null;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+
+    return currNode;
+  }
+
+  remove(item){
+    if(!this.head){
+      return null;
+    }
+
+    if(this.head.value === item){
+      this.head = this.head.next;
+      return;
+    }
+
+    let currNode = this.head;
+    let previusNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== item)){
+      previusNode = currNode;
+      currNode = currNode.next;
+    }
+    if(currNode === null){
+      console.log('Item not Found');
+      return;
+    }
+    previusNode.next = currNode.next;
+  }
+}
+
+module.exports = LinkedList;
--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const LanguageService = require('./language-service')
 const { requireAuth } = require('../middleware/jwt-auth')
+const LL = require('../Algorithms/LinkedList');
 
 const languageRouter = express.Router()
 
@@ -45,7 +46,7 @@ languageRouter
 
 languageRouter
   .get('/head', async (req, res, next) => {
-    try{
+    try {
       const language = await LanguageService.getUsersLanguage(
         req.app.get('db'),
         req.user.id,
@@ -54,33 +55,80 @@ languageRouter
         req.app.get('db'),
         req.language.id,
       )
-        // NOT DONE NEED TO FIGURE OUT HOW TO GO THROUGH LIST BETTER. 
-      const head = {
-        nextWord: words[0].original, // MAKE SURE TO LOOK AT WORDS
-        wordCorrectCount: words[language.head - 1].correct_count,
-        wordIncorrectCount: words[language.head -1].correct_count,
+      let head = {
+        nextWord: words[0].original,
+        wordCorrectCount: words[0].correct_count,
+        wordIncorrectCount: words[0].correct_count,
         totalScore: language.total_score,
- 
       }
-     
+      // needs to choose from the current word. 
+      // for(let i=0; i< words.length; i++){
+      //   if(words[i-1].id === words.word.id){
+      //     head = {
+      //       nextWord: words[i].original, 
+      //       wordCorrectCount: words[i - 1].correct_count,
+      //       wordIncorrectCount: words[i - 1].correct_count,
+      //       totalScore: language.total_score,
+      //     }
+      //   }
+      // }
+
       res
         .status(200)
         .json(head)
         .send()
-      
+
       next()
 
-    } catch (error){
+    } catch (error) {
       next(error)
     }
   })
 
 languageRouter
   .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
+    try {
+      const guess = req.body;
+
+      const language = await LanguageService.getUsersLanguage(
+        req.app.get('db'),
+        req.user.id,
+      )
+      const words = await LanguageService.getLanguageWords(
+        req.app.get('db'),
+        req.language.id,
+      )
+      let current = 0;
+      let isTrue = false;
+      const head = {
+        answer: words[current].translation,
+        isCorrect: isTrue,
+        nextWord: words[current].original,
+        totalScore: language.total_score,
+        wordCorrectCount: words[current].correct_count,
+        wordIncorrectCount: words[current].incorrect_count,
+      }
+
+      if (words[current].translation === guess) {
+        isTrue = true;
+        language.total_score++;
+        //words[current].correct_count++;
+      }else{
+        isTrue = false;
+       // words[current].incorrect_count++;
+      }
+    
+      res
+        .status(200)
+        .json(head)
+        .send();
+
+      next();
+
+    } catch (error) {
+      next(error)
+    }
+
   })
-
-
 
 module.exports = languageRouter;
